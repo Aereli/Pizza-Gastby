24aafb5e2f2b6e6b10a712b4b2fb2a744f694b5a
Eric <eric.oyanadel@nbcuni.com>
2020-12-03 11:35:46 -0500

    setup homepage via sanity backened

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,10 +1,49 @@
 import React from 'react'
+import { HomeContainerStyles, HomePageGrid } from '../styles/HomePageStyles'
+import LoadingGrid from '../components/LoadingGrid'
+import useLatestData from '../utils/useLatestData'
+import ItemGrid from '../components/ItemGrid';
+
+function CurrentlySlicing({ slicemasters }) {
+  return (
+    <div>
+      <h2 className="center">
+        <span className="mark tilt">Slicemasters On</span>
+      </h2>
+      <p>Standing by, ready to slice you up!</p>
+      {!slicemasters && <LoadingGrid count={4} />}
+      {slicemasters && !slicemasters?.length && (
+        <p>No one is working right now!</p>
+      )}
+      {slicemasters?.length && <ItemGrid items={slicemasters} />}
+    </div>
+  );
+}
+function HotSlices({ hotSlices }) {
+  return (
+    <div>
+      <h2 className="center">
+        <span className="mark tilt">Hot Slices!</span>
+      </h2>
+      <p>Come on by, buy the slice!</p>
+      {!hotSlices && <LoadingGrid count={4} />}{' '}
+      {hotSlices && !hotSlices?.length && <p>Nothin' in the Case</p>}
+      {hotSlices?.length && <ItemGrid items={hotSlices} />}
+    </div>
+  );
+}
 
 const HomePage = () => {
+  const { slicemasters, hotSlices } = useLatestData()
   return (
-    <>
-      <h1>homepage</h1>
-    </>
+    <HomeContainerStyles>
+      <h1>Your Local Pizza Spot</h1>
+      <p>Open Late!</p>
+      <HomePageGrid>
+        <CurrentlySlicing slicemasters={slicemasters} />
+        <HotSlices hotSlices={hotSlices} />
+      </HomePageGrid> 
+    </HomeContainerStyles>
   )
 }
 
---
+++ sanity/schemas/storeSettings.js
@@ -0,0 +1,28 @@
+export default {
+  // Computer Name
+  name: 'storeSettings',
+  // visible title
+  title: 'Settings',
+  type: 'document',
+  icon: () => '🏣',
+  fields: [
+    {
+      name: 'name',
+      title: 'Store Name',
+      type: 'string',
+      description: 'Name of the store',
+    },
+    {
+      name: 'slicemaster',
+      title: 'Slicemasters Currently Slicing',
+      type: 'array',
+      of: [{ type: 'reference', to: [{ type: 'person' }] }],
+    },
+    {
+      name: 'hotSlices',
+      title: 'Hot Slices available in the case',
+      type: 'array',
+      of: [{ type: 'reference', to: [{ type: 'pizza' }] }],
+    },
+  ],
+};
